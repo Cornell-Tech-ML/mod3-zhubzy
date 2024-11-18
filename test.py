ed8d28561d1fa50f813be96fc1bc9616ccb6b62f
Zach Zhong <zzyzsy0516321@yahoo.com>
2024-11-18 03:50:18 -0500

    ds

--- test.py
+++ test.py
@@ -4,10 +4,13 @@ CudaBackedn = TensorBackend(CudaOps)
 tensor1 = tensor([[[0.00,0.00],[0.00,0.00]],[[0.00,0.00],[0.00,1.00]]],FastTensorBackend)
 tensor2 = tensor([[[0.00,0.00],[0.00,1.00]]],FastTensorBackend)
 
-print("Fast ", ((tensor1.view(2,2,2,1) * tensor2.view(1,1,2,2))))
+print("FastA ", ((tensor1.view(2,2,2,1))))
+print("FastB ", tensor2.view(1,1,2,2))
+print("Fast Mul ", ((tensor1.view(2,2,2,1) * tensor2.view(1,1,2,2))))
 
 
 tensor1 = tensor([[[0.00,0.00],[0.00,0.00]],[[0.00,0.00],[0.00,1.00]]],CudaBackedn)
 tensor2 = tensor([[[0.00,0.00],[0.00,1.00]]],CudaBackedn)
-print("Cuda ", ((tensor1.view(2,2,2,1) * tensor2.view(1,1,2,2))))
-print("Cuda ", tensor2.sum(2).view(2,2,2))
+print("CudaA ", ((tensor1.view(2,2,2,1))))
+print("CudaB ", tensor2.view(1,1,2,2))
+print("Cuda Mul ", ((tensor1.view(2,2,2,1) * tensor2.view(1,1,2,2))))
