9f728b8f042911cc4b95f09aa495623535b4c89d
Zach Zhong <zzyzsy0516321@yahoo.com>
2024-11-18 03:41:21 -0500

    t

--- test.py
+++ test.py
@@ -1,13 +1,21 @@
 from minitorch import tensor, TensorBackend, FastOps,CudaOps
 FastTensorBackend = TensorBackend(FastOps)
 CudaBackedn = TensorBackend(CudaOps)
-tensor1 = tensor([[[0.00,0.00],[0.00,0.00]],[[0.00,0.00],[0.00,1.00]]],FastTensorBackend)
-tensor2 = tensor([[[0.00,0.00],[0.00,1.00]]],FastTensorBackend)
+tensor1 = tensor([
+    [[1, 2], [3, 4]],
+    [[5, 6], [7, 8]],
+    [[9, 10], [11, 12]]
+],FastTensorBackend)
 
-print("Fast ", ((tensor1.view(2,2,2,1) * tensor2.view(1,1,2,2)).sum(2)).view(2,2,2))
+
+print("Fast ", tensor1.sum(1))
+      
+tensor2 = tensor([
+    [[1, 2], [3, 4]],
+    [[5, 6], [7, 8]],
+    [[9, 10], [11, 12]]
+],CudaBackedn)
+print("Cuda ", tensor2.sum(1))
 
 
 
-tensor1 = tensor([[[0.00,0.00],[0.00,0.00]],[[0.00,0.00],[0.00,1.00]]],CudaBackedn)
-tensor2 = tensor([[[0.00,0.00],[0.00,1.00]]],CudaBackedn)
-print("Cuda ", ((tensor1.view(2,2,2,1) * tensor2.view(1,1,2,2)).sum(2)).view(2,2,2))
